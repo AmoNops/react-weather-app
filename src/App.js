fc9b15b62f163c0e58b497295039dcec4fab3cc6
AmoNops <162130641+AmoNops@users.noreply.github.com>
2024-04-09 23:11:07 +0200

    Set up react app and deleted unnecessary information

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/AmoNops" target="_blank">
+            Amogelang Sekhitla
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AmoNops/react-weather-app"
+            target="_blank"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
