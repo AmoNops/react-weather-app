80b37f6a85808dfda5c3832e1b772e48d7c6b091
AmoNops <162130641+AmoNops@users.noreply.github.com>
2024-04-11 21:24:17 +0200

    Created and added the structure of a one day forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <span className="img">
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={52}/>
           </span>
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
